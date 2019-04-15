d0cd2448d9e904dd5739e56fc7c299723b3bb3f2
Desire Brady <35373846+Desirebrady@users.noreply.github.com>
2019-04-15 15:21:03 +0200

    Add files via upload

---
+++ Day00/ex01.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "What can i do for you" << std::endl;
+    
+    std::string input;
+    std::cin >> input;
+    
+    if (input == "ADD")
+    {
+              std::cout << "Enter First Name" << std::endl; 
+    }
+    return 0;
+}
