2cf37c03bb9c851d9cd0aac9b16a88cf9c55339d
Desire SHUMBA <dshumba@c4r15s10.wethinkcode.co.za>
2019-06-05 10:06:40 +0200

    fixed for push

--- day01/ex05/main.cpp
+++ day01/ex05/main.cpp
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dshumba <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/06/05 09:57:33 by dshumba           #+#    #+#             */
+/*   Updated: 2019/06/05 09:57:56 by dshumba          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <iostream>
 #include "Human.hpp"
 #include "Brain.hpp"
@@ -6,5 +18,5 @@ int main()
 {
 	Human bob;
 	std::cout << bob.identify() << std::endl;
-	//std::cout << bob.getBrain().identify() << std::endl;
-}
+	std::cout << bob.getBrain().identify() << std::endl;
+}
